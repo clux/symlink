5db88047344948de9ea44c0e340e61ec0a5b437d
clux <analsandblaster@gmail.com>
2015-03-28 09:58:03 +0000

    last error condition test + forgot to update cyclical test to reflect 3 cycle

--- test/throw.test.js
+++ test/throw.test.js
@@ -3,13 +3,28 @@ var join = require('path').join;
 
 exports.cyclical = function (t) {
   // symlink test/cyclicals -d
-  var expct = 'cannot link cyclically dependent: ["module6","module7"]';
+  var expct = 'cannot link cyclically dependent: ["module6","module7","module8"]';
   symlink([ join(__dirname, 'cyclicals') ], [], function (err, cmds) {
     if (!err) {
       t.ok(false, "error was not set");
     }
     if (err) {
       t.equal(err.message, expct, "expected error");
+      t.equal(cmds, undefined, "no cmds output");
+    }
+    t.done();
+  });
+};
+
+exports.baddir = function (t) {
+  // symlink test/noent -d
+  var expct = "ENOENT, readdir '/home/clux/repos/symlink/test/noent'";
+  symlink([ join(__dirname, 'noent') ], [], function (err, cmds) {
+    if (!err) {
+      t.ok(false, "error was not set");
+    }
+    if (err) {
+      t.equal(err.message.slice(0, 16), expct.slice(0, 16), "expected error");
       t.equal(cmds, undefined, "no cmds output");
     }
     t.done();
