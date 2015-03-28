8c680e710a16d5a430ec59b6046881a664aae913
clux <analsandblaster@gmail.com>
2015-03-28 13:10:48 +0000

    restructure so coverage is more accurate (cli needs more testing)

--- bin.js
+++ bin.js
@@ -14,42 +14,4 @@ var argv = require('yargs')
   .help('h')
   .argv;
 
-// can do multiple unnamed arguments for directories relative to cwd
-var join =  require('path').join;
-var dirs = argv._.map(function (dir) {
-  return join(process.cwd(), dir);
-});
-var cp = require('child_process');
-var async = require('async');
-
-require('./')(dirs, argv.g || [], function (err, cmds) {
-  if (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-
-  if (argv.d) { // dry run
-    console.log(JSON.stringify(cmds, null, " "));
-  }
-  else {
-    // create one cp function cmd that execs and cbs to next in async series
-    var execs = cmds.map(function (cmd) {
-      return function (cb) {
-        console.log(cmd);
-        cp.exec(cmd, function (error, stdout) {
-          console.log(stdout);
-          cb(error);
-        });
-      };
-    });
-
-    // exec commands synchronously
-    async.series(execs, function (err) {
-      if (err) {
-        console.error(err.message);
-        process.exit(1);
-      }
-      process.exit(0);
-    });
-  }
-});
+require('./lib/cli').run(argv);
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-module.exports = process.env.SYMLINK_COV
-  ? require('./lib-cov/symlink.js')
-  : require('./lib/symlink.js');
+module.exports = process.env.SYMLINK_COV ?
+  require('./lib-cov/symlink.js'):
+  require('./lib/symlink.js');
+
+module.exports.cli = process.env.SYMLINK_COV ?
+  require('./lib-cov/cli.js'):
+  require('./lib/cli.js');
